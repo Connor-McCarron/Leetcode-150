d85360b08a8bd3d02e6049244c7dc8ab180e40b5
Connor Mc Carron <cmccarron140601@gmail.com>
2023-04-12 00:08:55 +0100

    Added Template Project Solution

---
+++ src/main/java/com/leet/Solution.java
@@ -0,0 +1,9 @@
+package com.leet;
+
+public class Solution
+{
+    public static void main( String[] args )
+    {
+       
+    }
+}
---
+++ src/test/java/com/leet/SolutionTest.java
@@ -0,0 +1,15 @@
+package com.leet;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SolutionTest 
+{
+
+    @Test
+    public void t1()
+    {
+        
+    }
+}
